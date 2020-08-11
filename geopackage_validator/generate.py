9c02162474507611e3dd38d104480aa083b72e20
Daan van Etten <daan.vanetten@kadaster.nl>
2020-08-11 10:14:29 +0200

    Fixes #5

--- geopackage_validator/generate.py
+++ geopackage_validator/generate.py
@@ -16,35 +16,42 @@ class Column(TypedDict):
     column_name: str
     geometry_type_name: str
     projection: str
+    data_type: str
 
 
 def generate_table_definitions(
     dataset: DataSource,
 ) -> Dict[str, Dict[str, List[Column]]]:
-    tables = dataset.ExecuteSQL(
+    geometry_tables = dataset.ExecuteSQL(
         "SELECT distinct table_name FROM gpkg_geometry_columns;"
     )
 
     tablelist = {}
-    for table in tables:
+    for table in geometry_tables:
         columns = dataset.ExecuteSQL(
             "SELECT column_name, geometry_type_name, srs_id FROM gpkg_geometry_columns where table_name = '{table_name}';".format(
                 table_name=table[0]
             )
         )
-        columnlist = []
-        for column in columns:
-            columnlist.append(
+        info = dataset.ExecuteSQL(
+            "PRAGMA TABLE_INFO('{table_name}');".format(table_name=table[0])
+        )
+        columnlist = [Column(column_name=item[1], data_type=item[2]) for item in info]
+        columnlist.extend(
+            [
                 Column(
                     column_name=column[0],
                     geometry_type_name=column[1],
                     projection=column[2],
                 )
-            )
+                for column in columns
+            ]
+        )
 
         tablelist[table[0]] = {"table_name": table[0], "columns": columnlist}
         dataset.ReleaseResultSet(columns)
-    dataset.ReleaseResultSet(tables)
+        dataset.ReleaseResultSet(info)
+    dataset.ReleaseResultSet(geometry_tables)
 
     return tablelist
 
--- geopackage_validator/validations/test_table_definitions_check.py
+++ geopackage_validator/validations/test_table_definitions_check.py
@@ -21,14 +21,16 @@ def test_table_definitions_check_correct():
 def test_table_definitions_check_incorrect_geometry():
     current_definitions = {
         "test_allcorrect": {
-            "table_name": "test_allcorrect",
             "columns": [
+                {"column_name": "fid", "data_type": "INTEGER"},
+                {"column_name": "geometry", "data_type": "POLYGON"},
                 {
                     "column_name": "geometry",
                     "geometry_type_name": "POINT",
                     "projection": 4326,
-                }
+                },
             ],
+            "table_name": "test_allcorrect",
         }
     }
 
@@ -42,7 +44,7 @@ def test_table_definitions_check_incorrect_geometry():
             "validation": "Geopackage must conform to given JSON definitions",
         },
         "errors": [
-            "Difference: Value of root['test_allcorrect']['columns'][0]['geometry_type_name'] changed from \"POLYGON\" to \"POINT\"."
+            "Difference: Value of root['test_allcorrect']['columns'][2]['geometry_type_name'] changed from \"POLYGON\" to \"POINT\"."
         ],
     }
 
@@ -50,14 +52,16 @@ def test_table_definitions_check_incorrect_geometry():
 def test_table_definitions_check_incorrect_projection():
     current_definitions = {
         "test_allcorrect": {
-            "table_name": "test_allcorrect",
             "columns": [
+                {"column_name": "fid", "data_type": "INTEGER"},
+                {"column_name": "geometry", "data_type": "POLYGON"},
                 {
                     "column_name": "geometry",
                     "geometry_type_name": "POLYGON",
                     "projection": 4323,
-                }
+                },
             ],
+            "table_name": "test_allcorrect",
         }
     }
 
@@ -71,7 +75,7 @@ def test_table_definitions_check_incorrect_projection():
             "validation": "Geopackage must conform to given JSON definitions",
         },
         "errors": [
-            "Difference: Value of root['test_allcorrect']['columns'][0]['projection'] changed from 4326 to 4323."
+            "Difference: Value of root['test_allcorrect']['columns'][2]['projection'] changed from 4326 to 4323."
         ],
     }
 
@@ -79,14 +83,16 @@ def test_table_definitions_check_incorrect_projection():
 def test_table_definitions_check_incorrect_column_name():
     current_definitions = {
         "test_allcorrect": {
-            "table_name": "test_allcorrect",
             "columns": [
+                {"column_name": "fid", "data_type": "INTEGER"},
+                {"column_name": "geom", "data_type": "POLYGON"},
                 {
-                    "column_name": "geom",
+                    "column_name": "geometry",
                     "geometry_type_name": "POLYGON",
                     "projection": 4326,
-                }
+                },
             ],
+            "table_name": "test_allcorrect",
         }
     }
 
@@ -100,7 +106,7 @@ def test_table_definitions_check_incorrect_column_name():
             "validation": "Geopackage must conform to given JSON definitions",
         },
         "errors": [
-            "Difference: Value of root['test_allcorrect']['columns'][0]['column_name'] changed from \"geometry\" to \"geom\"."
+            "Difference: Value of root['test_allcorrect']['columns'][1]['column_name'] changed from \"geometry\" to \"geom\"."
         ],
     }
 
