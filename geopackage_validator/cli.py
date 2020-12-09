3c48e5580cea03cabce9f122e21c331107640b9c
Roel van den Berg <roelvdberg@gmail.com>
2020-12-09 14:17:26 +0100

    Remove double output.

--- geopackage_validator/cli.py
+++ geopackage_validator/cli.py
@@ -154,8 +154,7 @@ def geopackage_validator_command(
                     validations_path,
                     validations,
                 )
-    except Exception as e:
-        logger.exception("Unknown error: %s", e)
+    except Exception:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         log_output(
             result_format(
