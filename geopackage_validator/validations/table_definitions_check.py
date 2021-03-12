b1e026dffe5dcc6258b3c75e15226d700d7fd709
Roel van den Berg <roelvdberg@gmail.com>
2021-03-12 14:10:31 +0100

    Rename table definition column data_type to type.

--- geopackage_validator/validations/table_definitions_check.py
+++ geopackage_validator/validations/table_definitions_check.py
@@ -5,6 +5,10 @@ from geopackage_validator.validations import validator
 from geopackage_validator.generate import generate_table_definitions
 
 
+LEGACY_COLUMN_TYPE_NAME = "data_type"
+COLUMN_TYPE_NAME = "type"
+
+
 def prepare_comparison(new_, old_):
     new_dict = {item["name"]: item for item in new_}
     old_dict = {item["name"]: item for item in old_}
@@ -14,7 +18,9 @@ def prepare_comparison(new_, old_):
     return new_dict, old_dict, ", ".join(missing), ", ".join(added), intersection
 
 
-def compare_column_definitions(new_columns, old_columns, table_name):
+def compare_column_definitions(
+    new_columns, old_columns, table_name, type_name=COLUMN_TYPE_NAME
+):
     assert (
         old_columns is not None
     ), f"table {table_name} in table definition misses columns"
@@ -29,12 +35,19 @@ def compare_column_definitions(new_columns, old_columns, table_name):
         result.append(f"table {table_name} has extra column(s): {added}")
 
     wrong_types = [
-        f"table {table_name}, column {k} changed type {old_dict[k]['data_type']} to {new_dict[k]['data_type']}"
+        f"table {table_name}, column {k} changed type {old_dict[k][type_name]} to {new_dict[k][COLUMN_TYPE_NAME]}"
         for k in intersection
-        if old_dict[k]["data_type"] != new_dict[k]["data_type"]
+        if old_dict[k][type_name] != new_dict[k][COLUMN_TYPE_NAME]
     ]
 
     return result + wrong_types
+
+
+def get_column_type_name(version):
+    version_tuple = tuple(int(v) for v in version.split("."))
+    if version_tuple <= (0, 5, 8):
+        return LEGACY_COLUMN_TYPE_NAME
+    return COLUMN_TYPE_NAME
 
 
 def compare_table_definitions(new_definition, old_definition, compare_columns=True):
@@ -56,6 +69,9 @@ def compare_table_definitions(new_definition, old_definition, compare_columns=Tr
             f"different projections: {new_projection} changed to {old_projection}"
         )
 
+    column_type_name = get_column_type_name(
+        old_definition.get("geopackage_validator_version", "0")
+    )
     for table_name in intersection:
         old_table = old_tables[table_name]
         new_table = new_tables[table_name]
@@ -67,7 +83,10 @@ def compare_table_definitions(new_definition, old_definition, compare_columns=Tr
             )
         if compare_columns:
             results += compare_column_definitions(
-                new_table["columns"], old_table.get("columns"), table_name
+                new_table["columns"],
+                old_table.get("columns"),
+                table_name,
+                column_type_name,
             )
 
     return results
