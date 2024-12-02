25dd228216f94cf78a0e338e585190ffc1699aaf
Michiel Korpel <michiel.korpel@kadaster.nl>
2024-12-02 19:51:33 +0100

    Add tests for RQ24

--- tests/validations/test_geometry_empty_check.py
+++ tests/validations/test_geometry_empty_check.py
@@ -6,20 +6,20 @@ from geopackage_validator.validations.geometry_empty_check import (
 
 
 def test_with_gpkg_empty():
-    dataset = open_dataset('tests/data/test_geometry_empty.gpkg')
+    dataset = open_dataset("tests/data/test_geometry_empty.gpkg")
     checks = list(query_geometry_empty(dataset, SQL_EMPTY_TEMPLATE))
     assert len(checks) == 1
-    assert checks[0][0] == 'stations'
-    assert checks[0][1] == 'geom'
+    assert checks[0][0] == "stations"
+    assert checks[0][1] == "geom"
     assert checks[0][2] == 45
     assert checks[0][3] == 129
 
 
 def test_with_gpkg_allcorrect():
-    dataset = open_dataset('tests/data/test_allcorrect.gpkg')
+    dataset = open_dataset("tests/data/test_allcorrect.gpkg")
     checks = list(query_geometry_empty(dataset, SQL_EMPTY_TEMPLATE))
     assert len(checks) == 1
-    assert checks[0][0] == 'test_allcorrect'
-    assert checks[0][1] == 'geom'
+    assert checks[0][0] == "test_allcorrect"
+    assert checks[0][1] == "geom"
     assert checks[0][2] == 0
     assert checks[0][3] is None
