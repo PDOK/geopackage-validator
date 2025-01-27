1307cf794f339ff7869fce9c70c578baeebb4107
Damien Mulder <damien.mulder@kadaster.nl>
2025-01-27 14:58:05 +0100

    Check for spatialite geometry

--- tests/validations/test_geometry_valid_check.py
+++ tests/validations/test_geometry_valid_check.py
@@ -1,20 +1,21 @@
 from geopackage_validator.utils import open_dataset
 from geopackage_validator.validations.geometry_valid_check import (
     query_geometry_valid,
-    SQL_ONLY_VALID_TEMPLATE,
+    SQL_VALID_TEMPLATE_V0,
     SQL_VALID_TEMPLATE,
 )
 
 
 def test_with_gpkg_valid():
     dataset = open_dataset("tests/data/test_geometry_valid.gpkg")
-    checks = list(query_geometry_valid(dataset, SQL_ONLY_VALID_TEMPLATE))
-    assert len(checks) == 1
-    assert checks[0][0] == "test_geometry_valid"
-    assert checks[0][1] == "geometry"
-    assert checks[0][2] == "Self-intersection"
-    assert checks[0][3] == 1
-    assert checks[0][4] == 1
+    for template in [SQL_VALID_TEMPLATE_V0, SQL_VALID_TEMPLATE]:
+        checks = list(query_geometry_valid(dataset, template))
+        assert len(checks) == 1
+        assert checks[0][0] == "test_geometry_valid"
+        assert checks[0][1] == "geometry"
+        assert checks[0][2] == "Self-intersection"
+        assert checks[0][3] == 1
+        assert checks[0][4] == 1
 
 
 def test_with_gpkg_simple():
@@ -51,6 +52,19 @@ def test_with_gpkg_null():
     dataset = open_dataset("tests/data/test_geometry_null.gpkg")
     checks = list(query_geometry_valid(dataset, SQL_VALID_TEMPLATE))
     assert len(checks) == 0
+
+
+def test_with_gpkg_spatialite():
+    # spatialite geometries are not considered valid
+    dataset = open_dataset("tests/data/test_geometry_spatialite.gpkg")
+    for template in [SQL_VALID_TEMPLATE_V0, SQL_VALID_TEMPLATE]:
+        checks = list(query_geometry_valid(dataset, template))
+        assert len(checks) == 1
+        assert checks[0][0] == "test_geometry_spatialite"
+        assert checks[0][1] == "geom"
+        assert checks[0][2] == "Not GeoPackage geometry"
+        assert checks[0][3] == 1
+        assert checks[0][4] == 1
 
 
 def test_with_gpkg_allcorrect():
